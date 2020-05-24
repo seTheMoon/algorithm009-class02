165fe221ac1195eac6b0f89403ca1dd2b67a10bc
hehe <374086276@qq.com>
2020-05-24 18:23:49 +0800

    第一周作业提交

---
+++ Week_01/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int trap(int[] height) {
+        Stack<Integer> stack = new Stack();
+        int sum = 0;
+        for (int i = 0; i < height.length; i++) {
+            while ( !stack.isEmpty() && height[i] > height[stack.peek()]) {
+               int pillarHeight = height[stack.pop()];
+               if (stack.isEmpty()) break;
+               int containerHeitht = Math.min(height[i],height[stack.peek()]) - pillarHeight;
+               int distance = i - stack.peek() - 1;
+               sum += distance * containerHeitht;
+            }
+            stack.push(i);
+        }
+        return sum;
+    }
+}
